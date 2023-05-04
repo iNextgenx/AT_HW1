df2ddecec4a7ae206426056d47c51b215d7d973a
Igor Smirnov <inextgen@mail.ru>
2023-05-04 23:51:55 +0300

    TestNG tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testWhenAllCorrect() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 400;
+        int actual = service.remain(600);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase0() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase1() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase999() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase1001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenPurchase2001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(2001);
+        Assert.assertEquals(expected, actual);
+    }
+}
